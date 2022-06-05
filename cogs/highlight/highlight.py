4bc821815822072d3783e0f32135b70c9170ae31
Injabie3 <injabie3@gmail.com>
2022-06-04 21:36:07 -0700

    [Highlight] Amend _triggeredUpdate docstring

--- cogs/highlight/highlight.py
+++ cogs/highlight/highlight.py
@@ -618,6 +618,8 @@ class Highlight(commands.Cog):
     ):
         """Updates the last time a user had their words triggered in a channel.
 
+        This sets self.lastTriggered[sid][cid][uid] to the specified datetime.
+
         Parameters:
         -----------
         channel: discord.TextChannel
@@ -626,10 +628,6 @@ class Highlight(commands.Cog):
             The user that we wish to update.
         timestamp: datetime.datetime
             The timestamp we wish to update.
-
-        Returns:
-        --------
-        None, updates self.lastTriggered[sid][cid][uid] with the newest datetime.
         """
         sid = channel.guild.id
         cid = channel.id
